168aace683958e620df6c4f9977851e1cf0d2aec
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-11-29 12:59:22 -0600

    Initial Flat List made for storm data | fake storm data created
    Initial Flat List made for storm data | fake storm data created

--- src/components/App.js
+++ src/components/App.js
@@ -10,27 +10,68 @@ import React from 'react';
 import { 
   ScrollView, 
   View,
-  Text, 
+  Text,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  // TouchableOpacity
  } from 'react-native';
+import {
+  Card,
+  Title,
+  Paragraph,
+} from 'react-native-paper'
 import { 
   Appbar, 
 } from 'react-native-paper';
+import data from '../data/data';
 
 
-
+const Item = ({ title, category, summary }) => (
+  <Card>
+    <Card.Content>
+      <Title>{title} | Category: {category}</Title>
+      <Paragraph>{summary}</Paragraph>
+    </Card.Content>
+  </Card>
+);
 
 
 export default function App () {
 
+  const renderItem = ({ item }) => (
+    <Item title={item.title} category={item.category} summary={item.summary} />
+  );
 
-  // render() {
+
+
     return (
-      <ScrollView>
+      <SafeAreaView>
         <Appbar.Header>
           <Appbar.Content title="Storm Album" />
         </Appbar.Header>
         <View>
-          <Text>Hello World</Text>
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+          />
         </View>
-      </ScrollView>
+      </SafeAreaView>
     )};
+
+    const styles = StyleSheet.create({
+      // container: {
+      //   flex: 1,
+      //   marginTop: StatusBar.currentHeight || 0,
+      // },
+      item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },
+      title: {
+        fontSize: 32,
+      },
+    });
---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        title: 'Storm One',
+        category: '1',
+        summary: 'Here is a summary of this storm.'
+    },
+    {
+        title: 'Storm Two',
+        category: '1',
+        summary: 'Here is a summary of this storm.'
+    },
+    {
+        title: 'Storm Three',
+        category: '1',
+        summary: 'Here is a summary of this storm.'
+    },
+    {
+        title: 'Storm Four',
+        category: '1',
+        summary: 'Here is a summary of this storm.'
+    },
+    {
+        title: 'Storm Five',
+        category: '1',
+        summary: 'Here is a summary of this storm.'
+    }
+]
+
+export default data
